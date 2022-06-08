2e7e627a6ab369bd2538701403f0f77cd972ab52
Павел Сотников <pax4400@mail.ru>
2022-06-08 22:41:04 +0300

    "2.1new"

--- app/src/main/java/ru/netology/nmedia/dto/Post.kt
+++ app/src/main/java/ru/netology/nmedia/dto/Post.kt
@@ -5,20 +5,7 @@ data class Post(
     val author: String,
     val content: String,
     val published: String,
-    var likedByMe: Boolean = false,
-    var countLikes: UInt,
-    var countShare: UInt,
-    var countGlaz: UInt
-) {
-    fun uIntToString(n:UInt):String {
-        val res = when {
-            n < 1000u -> n.toString()
-            n < 10000u -> "${n/1000u}.${n/100u%10u}K"
-            n < 1000000u -> "${n/1000u}K"
-            n < 10000000u -> "${n/1000000u}.${n/100000u%10u}M"
-            n < 100000000u -> "${n/1000000u}M"
-            else -> "очМного"
-        }
-        return res
-    }
-}
+    val likes:Int = 0,
+    val shares:Int = 0,
+    var likedByMe: Boolean = false
+)
